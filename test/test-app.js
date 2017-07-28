589c8cb9ece62700e19bf388f5f5c7c806a0cba5
Thomaz Moura <padilha.thomaz@gmail.com>
2017-07-28 00:04:48 -0300

    Acréscimo de testes do Mocha

---
+++ test/test-app.js
@@ -0,0 +1,16 @@
+'use strict';
+var path = require('path');
+var assert = require('yeoman-generator').assert;
+var helpers = require('yeoman-generator').test;
+
+describe('dotnetsolution', function() {
+    describe('default', function() {
+        before(function() {
+
+        });
+
+        it('create files', function() {
+
+        });
+    });
+});
