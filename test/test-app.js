9061506567eb8ec77eae7d1a2703f9f6f654ee9d
Thomaz Moura <padilha.thomaz@gmail.com>
2017-07-29 12:15:20 -0300

    Reestrutução do teste

--- test/test-app.js
+++ test/test-app.js
@@ -4,30 +4,27 @@ var assert = require('yeoman-assert');
 var path = require('path');
 
 describe('dotnetsolution', function() {
-    describe('default', function() {
-        before(function(done) {
-            helpers.run(path.join(__dirname, '../app'))
-                .withPrompts({ solutionName: 'TestSolution' })
-                .withOptions({ skipInstall: true })
-                .on('end', done)
-        });
+    before(function(done) {
+        helpers.run(path.join(__dirname, '../app'))
+            .withPrompts({ solutionName: 'TestSolution' })
+            .withOptions({ skipInstall: true })
+            .on('end', done)
+    });
 
-        it('Cria os arquivos esperados', function() {
-            assert.file([
-                'TestSolution.sln',
-                'TestSolution.Dominio/Repositorios/IRepositorio.cs',
-                'TestSolution.Dominio/Objetos/Atributos/EntidadeAttribute.cs'
-            ]);
-        });
+    it('Cria os arquivos esperados', function() {
+        assert.file([
+            'TestSolution.sln',
+            'TestSolution.Dominio/Repositorios/IRepositorio.cs',
+            'TestSolution.Dominio/Objetos/Atributos/EntidadeAttribute.cs'
+        ]);
+    });
 
-        it('Edita o conteúdo como esperado', function() {
-            assert.fileContent([
-                ['TestSolution.sln', /TestSolution.Dominio/],
-                ['TestSolution.sln', /TestSolution.Dados/],
-                ['TestSolution.Dominio/Repositorios/IRepositorio.cs', /namespace TestSolution.Dominio.Repositorios/],
-                ['TestSolution.Dominio/Properties/AssemblyInfo.cs', /assembly: AssemblyTitle\("TestSolution.Dominio"\)/]
-            ]);
-
-        });
+    it('Edita o conteúdo como esperado', function() {
+        assert.fileContent([
+            ['TestSolution.sln', /TestSolution.Dominio/],
+            ['TestSolution.sln', /TestSolution.Dados/],
+            ['TestSolution.Dominio/Repositorios/IRepositorio.cs', /namespace TestSolution.Dominio.Repositorios/],
+            ['TestSolution.Dominio/Properties/AssemblyInfo.cs', /assembly: AssemblyTitle\("TestSolution.Dominio"\)/]
+        ]);
     });
 });
