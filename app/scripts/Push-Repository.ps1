98826cfe69395934e8e846551d0dbbd0f7e7b1d3
Thomaz Moura <padilha.thomaz@gmail.com>
2017-07-29 17:18:53 -0300

    Correção de script de atualização do GitHub

--- app/scripts/Push-Repository.ps1
+++ app/scripts/Push-Repository.ps1
@@ -3,9 +3,12 @@ Param(
   [string]$branch
 )
 
-$pushUrl =  "https://"$token"@github.com/thomazpadilha/Generator.DotNetSolution.git";
-$branchRef = "head:"$branch
+$pushUrl =  "https://$token@github.com/thomazpadilha/Generator.DotNetSolution.git";
+$branchRef = "head:$branch"
 
 if($branch.Contains("dev") || $branch.Contains("master")){
     git.exe push $pushUrl $branchRef;
+    Write-Host "Atualizando branch $branch do repositório no GitHub";
+}else{
+    Write-Host "A branch $branch não está sendo replicada no GitHub";  
 }
