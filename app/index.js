a2ad8e7ba2138a8db1c30f8baf67d71c0b5d1682
Thomaz Moura <padilha.thomaz@gmail.com>
2017-07-29 23:06:24 -0300

    Separação entre name e namespace

--- app/index.js
+++ app/index.js
@@ -6,6 +6,7 @@ var rename = require('gulp-rename');
 var replace = require('gulp-replace');
 var mkdirp = require('mkdirp');
 var fs = require('fs');
+var _ = require('lodash');
 var chalk = require('chalk');
 
 module.exports = class extends Generator {
@@ -18,13 +19,28 @@ module.exports = class extends Generator {
     prompting() {
         this.log(yosay('Bem-vindo ao gerador de solution .NET genérica com DDD!'))
 
+        var generator = this;
         return this.prompt([{
             type: "input",
             name: "solutionName",
-            message: "Nome da solution",
-            default: this.config.get("solutionName") || "CustomProject"
+            message: "Nome completo da solution - define o namespace",
+            default: this.config.get("solutionName") || "Sample.Project"
+        }, {
+            when: function(answers) {
+                var nameParts = answers.solutionName.split(".");
+                var multiNameParts = nameParts.length > 1;
+                generator.tempAnswers = multiNameParts ?
+                    _.last(nameParts) :
+                    answers.solutionName;
+                return multiNameParts;
+            },
+            type: "input",
+            name: "solutionShortName",
+            message: "Nome simplificado da solution",
+            default: this.tempAnswers
         }]).then((answers) => {
             this.config.set("solutionName", answers.solutionName);
+            this.config.set("solutionShortName", answers.solutionShortName || this.tempAnswers);
             this.config.save();
         });
     }
@@ -39,22 +55,30 @@ module.exports = class extends Generator {
                 this.templatePath(),
                 this.templatePath("../temp"), {
                     process: function(content) {
-                        var regEx = new RegExp(/InsertSolutionNamespaceHere/, 'g')
-                        return content.toString().replace(regEx, '<%= solutionName %>');
+                        var namespaceRegEx = new RegExp(/InsertSolutionNamespaceHere/, 'g')
+                        var nameRegEx = new RegExp(/InsertSolutionNameHere/, 'g')
+                        return content.toString()
+                            .replace(namespaceRegEx, '<%= solutionName %>')
+                            .replace(nameRegEx, '<%= solutionShortName %>');
                     }
                 }
             );
 
             var solutionName = this.config.get("solutionName");
+            var solutionShortName = this.config.get("solutionShortName");
             this.registerTransformStream(rename(function(path) {
                 path.basename = path.basename.replace(/(InsertSolutionNamespaceHere)/g, solutionName);
                 path.dirname = path.dirname.replace(/(InsertSolutionNamespaceHere)/g, solutionName);
+
+                path.basename = path.basename.replace(/(InsertSolutionNameHere)/g, solutionShortName);
+                path.dirname = path.dirname.replace(/(InsertSolutionNameHere)/g, solutionShortName);
             }));
 
             this.fs.copyTpl(
                 this.templatePath("../temp"),
                 this.destinationPath(), {
-                    solutionName: this.config.get("solutionName")
+                    solutionName: this.config.get("solutionName"),
+                    solutionShortName: this.config.get("solutionShortName"),
                 }
             );
 
--- test/test-app.js
+++ test/test-app.js
@@ -3,28 +3,105 @@ var helpers = require('yeoman-test');
 var assert = require('yeoman-assert');
 var path = require('path');
 
-describe('dotnetsolution', function() {
+describe('Namespace Test.Solution', function() {
     before(function(done) {
         helpers.run(path.join(__dirname, '../app'))
-            .withPrompts({ solutionName: 'TestSolution' })
+            .withPrompts({
+                solutionName: 'Test.Solution'
+            })
             .withOptions({ skipInstall: true })
             .on('end', done)
     });
 
     it('Cria os arquivos esperados', function() {
         assert.file([
-            'TestSolution.sln',
-            'TestSolution.Dominio/Repositorios/IRepositorio.cs',
-            'TestSolution.Dominio/Objetos/Atributos/EntidadeAttribute.cs'
+            'Test.Solution.sln',
+            'Test.Solution.Dominio/Repositorios/IRepositorio.cs',
+            'Test.Solution.Dominio/Objetos/Atributos/EntidadeAttribute.cs'
         ]);
     });
 
     it('Edita o conteúdo como esperado', function() {
         assert.fileContent([
-            ['TestSolution.sln', /TestSolution.Dominio/],
-            ['TestSolution.sln', /TestSolution.Dados/],
-            ['TestSolution.Dominio/Repositorios/IRepositorio.cs', /namespace TestSolution.Dominio.Repositorios/],
-            ['TestSolution.Dominio/Properties/AssemblyInfo.cs', /assembly: AssemblyTitle\("TestSolution.Dominio"\)/]
+            ['Test.Solution.sln', /Test.Solution.Dominio/],
+            ['Test.Solution.sln', /Test.Solution.Dados/],
+            ['Test.Solution.Dominio/Repositorios/IRepositorio.cs', /namespace Test.Solution.Dominio.Repositorios/],
+            ['Test.Solution.Dominio/Properties/AssemblyInfo.cs', /assembly: AssemblyTitle\("Test.Solution.Dominio"\)/]
+        ]);
+    });
+
+    it('Cria arquivos específicos com nome reduzido', function() {
+        assert.file([
+            'Test.Solution.Dados/SolutionContext.cs'
+        ]);
+    });
+});
+
+describe('Namespace Test.Solution Name Solucao', function() {
+    before(function(done) {
+        helpers.run(path.join(__dirname, '../app'))
+            .withPrompts({
+                solutionName: 'Test.Solution',
+                solutionShortName: 'Solucao'
+            })
+            .withOptions({ skipInstall: true })
+            .on('end', done)
+    });
+
+    it('Cria os arquivos esperados', function() {
+        assert.file([
+            'Test.Solution.sln',
+            'Test.Solution.Dominio/Repositorios/IRepositorio.cs',
+            'Test.Solution.Dominio/Objetos/Atributos/EntidadeAttribute.cs'
+        ]);
+    });
+
+    it('Edita o conteúdo como esperado', function() {
+        assert.fileContent([
+            ['Test.Solution.sln', /Test.Solution.Dominio/],
+            ['Test.Solution.sln', /Test.Solution.Dados/],
+            ['Test.Solution.Dominio/Repositorios/IRepositorio.cs', /namespace Test.Solution.Dominio.Repositorios/],
+            ['Test.Solution.Dominio/Properties/AssemblyInfo.cs', /assembly: AssemblyTitle\("Test.Solution.Dominio"\)/]
+        ]);
+    });
+
+    it('Cria arquivos específicos com nome reduzido', function() {
+        assert.file([
+            'Test.Solution.Dados/SolucaoContext.cs'
+        ]);
+    });
+});
+
+describe('Namespace Solution', function() {
+    before(function(done) {
+        helpers.run(path.join(__dirname, '../app'))
+            .withPrompts({
+                solutionName: 'Solution'
+            })
+            .withOptions({ skipInstall: true })
+            .on('end', done)
+    });
+
+    it('Cria os arquivos esperados', function() {
+        assert.file([
+            'Solution.sln',
+            'Solution.Dominio/Repositorios/IRepositorio.cs',
+            'Solution.Dominio/Objetos/Atributos/EntidadeAttribute.cs'
+        ]);
+    });
+
+    it('Edita o conteúdo como esperado', function() {
+        assert.fileContent([
+            ['Solution.sln', /Solution.Dominio/],
+            ['Solution.sln', /Solution.Dados/],
+            ['Solution.Dominio/Repositorios/IRepositorio.cs', /namespace Solution.Dominio.Repositorios/],
+            ['Solution.Dominio/Properties/AssemblyInfo.cs', /assembly: AssemblyTitle\("Solution.Dominio"\)/]
+        ]);
+    });
+
+    it('Cria arquivos específicos com nome reduzido', function() {
+        assert.file([
+            'Solution.Dados/SolutionContext.cs'
         ]);
     });
 });
